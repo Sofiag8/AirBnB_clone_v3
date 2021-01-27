c744cb67a415aa3ba478f83efdb9b42ab1b42ebd
santiagopinzonD <santiagopinzon456@gmail.com>
2021-01-27 19:12:20 +0000

    handler error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ starting api """
 from os import environ
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def close(self):
     """Clossing session method"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """handler error 404"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST', '0.0.0.0')
